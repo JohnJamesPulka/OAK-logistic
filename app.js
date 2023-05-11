a7ea5bc764523d68a1dfd75032b7f52b09ca1c50
john james <yagazhigler@gmail.com>
2023-05-11 15:54:58 +0100

    change all

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('menu-btn')
+const btn2 = document.getElementById('ss-nav-menu-id')
+
+
+// btn.addEventListener('click', ()=>{
+//     btn.classList.toggle('open')
+//     btn2.classList.toggle('hidden')
+// })
+
+btn.addEventListener('click', ()=>{
+    btn.classList.toggle('open')
+    btn2.classList.toggle('ss-nav-menu')
+})
